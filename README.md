008225ee988c7973071d2cb30fd9509095c55843
Glenn Rempe <glenn@rempe.us>
2020-04-27 16:09:42 -0700

    Simple benchmark comparing tweetnacl and sodium-native

---
+++ README.md
@@ -0,0 +1,11 @@
+# TweetNacl vs sodium-native benchmark
+
+## Run It
+
+```
+npm install
+node benchmark.js
+```
+
+For more usage and to see interpret the various outputs
+see [Benny](https://caderek.github.io/benny/)
---
+++ benchmark.js
@@ -0,0 +1,39 @@
+// https://caderek.github.io/benny/
+const b = require("benny")
+
+// setup tweetnacl and its keypair
+// https://github.com/dchest/tweetnacl-js
+const nacl = require("tweetnacl")
+const naclKeypair = nacl.sign.keyPair()
+
+// setup sodium-native
+// https://sodium-friends.github.io/docs/docs/signing
+const sodium = require("sodium-native")
+const pk = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
+const sk = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
+sodium.crypto_sign_keypair(pk, sk)
+
+// create a common message to sign
+const m = new Uint8Array(100)
+var i
+for (i = 0; i < m.length; i++) {
+  m[i] = i & 0xff
+}
+
+b.suite(
+  "tweetnacl vs. sodium-native",
+
+  b.add("sign w/ tweetnacl", () => {
+    let sm = nacl.sign(m, naclKeypair.secretKey)
+  }),
+
+  b.add("sign w/ sodium-native", () => {
+    let sm = Buffer.alloc(sodium.crypto_sign_BYTES + m.length)
+    sodium.crypto_sign(sm, m, sk)
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({ file: "sign", version: "1.0.0" }),
+  b.save({ file: "sign", format: "chart.html" })
+)
