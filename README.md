f9b2b374fef9ffad3fba33341db33900e35cd987
Glenn Rempe <glenn@truestamp.com>
2021-07-14 14:28:13 -0400

    Update README. Benchmark sign and verify. Benchmark PEM vs. KeyObject for Node crypto.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TweetNacl vs sodium-native benchmark
+# TweetNacl vs. sodium-native vs.  benchmark
 
 ## Run It
 
@@ -12,22 +12,58 @@ see [Benny](https://caderek.github.io/benny/)
 
 ## tl;dr
 
-```
-❯ node benchmark.js
-Running "tweetnacl vs. sodium-native" suite...
+`sodium-native` is the fastest choice, but with it comes a hard to fulfill dependency in some environments.
+
+It might be preferable to use the Node.js `crypto.sign()` and `crypto.verify()`, but they expect `PEM` encoded public and private keys. Is there a way to use raw keys? If `PEM` encoded keys are not rehydrated back to Node `KeyObject` form then the sign/verify operations can be much slower. `ed25519` signing is available in Node.js 12+ which is currently the oldest supported LTS version.
+
+`tweetnacl-js` makes a very good pure Javascript fallback for use in other environments or the browser, but it is by far the slowest option.
+
+Is the speed of sign/verify the most critical aspect when making a choice? Or reducing native compiled dependencies?
+
+```sh
+$ node benchmark.js
+Running "sign : tweetnacl vs. sodium-native vs. crypto" suite...
 Progress: 100%
 
   sign w/ tweetnacl:
-    286 ops/s, ±2.24%      | slowest, 99.47% slower
+    242 ops/s, ±3.19%      | slowest, 99.49% slower
 
   sign w/ sodium-native:
-    54 008 ops/s, ±0.62%   | fastest
+    47 315 ops/s, ±1.61%   | fastest
 
-Finished 2 cases!
+  sign w/ crypto (KeyObject):
+    23 310 ops/s, ±1.58%   | 50.73% slower
+
+  sign w/ crypto (PEM):
+    10 964 ops/s, ±2.11%   | 76.83% slower
+
+Finished 4 cases!
   Fastest: sign w/ sodium-native
   Slowest: sign w/ tweetnacl
 
 Saved to: benchmark/results/sign.json
 
 Saved to: benchmark/results/sign.chart.html
+Running "verify : tweetnacl vs. sodium-native vs. crypto" suite...
+Progress: 100%
+
+  verify w/ tweetnacl:
+    126 ops/s, ±3.29%      | slowest, 99.28% slower
+
+  verify w/ sodium-native:
+    17 494 ops/s, ±1.03%   | fastest
+
+  verify w/ crypto (KeyObject):
+    9 301 ops/s, ±1.02%    | 46.83% slower
+
+  verify w/ crypto (PEM):
+    8 582 ops/s, ±1.66%    | 50.94% slower
+
+Finished 4 cases!
+  Fastest: verify w/ sodium-native
+  Slowest: verify w/ tweetnacl
+
+Saved to: benchmark/results/verify.json
+
+Saved to: benchmark/results/verify.chart.html
 ```
--- benchmark.js
+++ benchmark.js
@@ -4,18 +4,46 @@ const b = require("benny")
 // setup tweetnacl and its keypair
 // https://github.com/dchest/tweetnacl-js
 const nacl = require("tweetnacl")
-const crypto = require("crypto")
 const naclKeypair = nacl.sign.keyPair()
 
 // setup sodium-native
 // https://sodium-friends.github.io/docs/docs/signing
 const sodium = require("sodium-native")
-const pk = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
-const sk = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
-sodium.crypto_sign_keypair(pk, sk)
+const sodiumPublicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
+const sodiumSecretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
+sodium.crypto_sign_keypair(sodiumPublicKey, sodiumSecretKey)
 
-// setup crypto
-const cryptoKeypair = crypto.generateKeyPairSync('ed25519')
+// setup Node.js crypto keypair
+// Example: https://gist.github.com/teiikushi07/40fa141237a763b8cf30d8c1095eb1c8
+const crypto = require("crypto")
+const cryptoKeypair = crypto.generateKeyPairSync("ed25519")
+
+// See : nodejs.org/api/crypto.html#crypto_keyobject_export_options
+// To inspect the ASN.1 PEM encoded key visit: https://lapo.it/asn1js/
+const cryptoPubKeyPem = cryptoKeypair.publicKey.export({
+  type: "spki",
+  format: "pem",
+})
+// console.log(cryptoPubKeyPem)
+
+// To inspect the ASN.1 PEM encoded key visit: https://lapo.it/asn1js/
+const cryptoPrivKeyPem = cryptoKeypair.privateKey.export({
+  type: "pkcs8",
+  format: "pem",
+})
+// console.log(cryptoPrivKeyPem)
+
+const cryptoPubKeyRehydrated = crypto.createPublicKey({
+  key: cryptoPubKeyPem,
+  format: "pem",
+  type: "spki",
+})
+
+const cryptoPrivKeyRehydrated = crypto.createPrivateKey({
+  key: cryptoPrivKeyPem,
+  format: "pem",
+  type: "pkcs8",
+})
 
 // create a common message to sign
 const m = new Uint8Array(100)
@@ -24,24 +52,73 @@ for (i = 0; i < m.length; i++) {
   m[i] = i & 0xff
 }
 
+// sign the message with nacl.sign for the verify step
+const naclTestSig = nacl.sign.detached(m, naclKeypair.secretKey)
+
+// sign the message with sodium.crypto_sign for the verify step
+let sodiumTestSig = Buffer.alloc(sodium.crypto_sign_BYTES + m.length)
+sodium.crypto_sign(sodiumTestSig, m, sodiumSecretKey)
+
+// sign the message with crypto.sign for the verify step
+const cryptoTestSig = crypto.sign(null, m, cryptoKeypair.privateKey)
+
 b.suite(
-  "tweetnacl vs. sodium-native vs. crypto",
+  "sign : tweetnacl vs. sodium-native vs. crypto",
 
   b.add("sign w/ tweetnacl", () => {
-    let sm = nacl.sign(m, naclKeypair.secretKey)
+    let sm = nacl.sign.detached(m, naclKeypair.secretKey)
   }),
 
   b.add("sign w/ sodium-native", () => {
     let sm = Buffer.alloc(sodium.crypto_sign_BYTES + m.length)
-    sodium.crypto_sign(sm, m, sk)
+    sodium.crypto_sign(sm, m, sodiumSecretKey)
   }),
 
-  b.add("sign w/ crypto", () => {
-    crypto.sign(null, m, cryptoKeypair.privateKey)
+  b.add("sign w/ crypto (KeyObject)", () => {
+    // crypto.sign(null, m, cryptoKeypair.privateKey)
+    crypto.sign(null, m, cryptoPrivKeyRehydrated)
+  }),
+
+  b.add("sign w/ crypto (PEM)", () => {
+    crypto.sign(null, m, cryptoPrivKeyPem)
   }),
 
   b.cycle(),
   b.complete(),
   b.save({ file: "sign", version: "1.0.0" }),
   b.save({ file: "sign", format: "chart.html" })
+)
+
+b.suite(
+  "verify : tweetnacl vs. sodium-native vs. crypto",
+
+  b.add("verify w/ tweetnacl", () => {
+    if (!nacl.sign.detached.verify(m, naclTestSig, naclKeypair.publicKey)) {
+      throw new Error("nacl.sign.detached.verify failed")
+    }
+  }),
+
+  b.add("verify w/ sodium-native", () => {
+    if (!sodium.crypto_sign_open(m, sodiumTestSig, sodiumPublicKey)) {
+      throw new Error("sodium.crypto_sign_open failed")
+    }
+  }),
+
+  b.add("verify w/ crypto (KeyObject)", () => {
+    // crypto.verify(null, m, cryptoKeypair.publicKey, cryptoTestSig)
+    if (!crypto.verify(null, m, cryptoPubKeyRehydrated, cryptoTestSig)) {
+      throw new Error("verify failed")
+    }
+  }),
+
+  b.add("verify w/ crypto (PEM)", () => {
+    if (!crypto.verify(null, m, cryptoPubKeyPem, cryptoTestSig)) {
+      throw new Error("verify failed")
+    }
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({ file: "verify", version: "1.0.0" }),
+  b.save({ file: "verify", format: "chart.html" })
 )
